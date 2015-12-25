d503934d5d8523fd5f355e81caea1ec04392a7c0
jiteshjha <jiteshjha96@gmail.com>
2015-12-26 00:47:18 +0530

    Added linkedlist print reverse

---
+++ DataStructures/linkedlist/linkedlist_printreverse.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+  int data;
+  struct node * next;
+}NODE;
+
+typedef NODE * NODEPTR;
+
+NODEPTR newNode(int data) {
+  NODEPTR temp = (NODEPTR)malloc(sizeof(NODE));
+  temp -> data = data;
+  temp -> next = NULL;
+  return temp;
+}
+NODEPTR push(NODEPTR head, int data) {
+  NODEPTR temp = newNode(data);
+  temp -> next = head;
+  head = temp;
+  return head;
+}
+
+void display(NODEPTR head) {
+  if(head == NULL) {
+    printf("List empty!\n");
+    return;
+  }
+  NODEPTR temp = head;
+  while(temp != NULL) {
+    printf("%d\n", temp -> data);
+    temp = temp -> next;
+  }
+}
+
+void printReverse(NODEPTR head) {
+  if(head == NULL)
+    return;
+  printReverse(head -> next);
+  printf("%d\t", head -> data);
+}
+int main() {
+  NODEPTR head = NULL;
+
+  head = push(head, 2);
+  head = push(head, 3);
+  head = push(head, 4);
+  head = push(head, 5);
+  head = push(head, 6);
+  display(head);
+  printReverse(head);
+  return 0;
+}
